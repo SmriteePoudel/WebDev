434067e19d7a040ca4e58e4bb6c9cbe78a944c1d
Smritee <esmritypoudel@gmail.com>
2025-02-17 15:45:14 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-alert(" WELCOME TO MY WEBSITE")
+alert("Hello World");
+ console.log(" Code is running...")
+
+ var a=prompt("Enter your number")
+ var isTrue =confirm("Your Computer is Lagging dont leave this page if u leave it will burst...")
+ if (isTrue){
+    console.log("Computer is bursting")
+
+
+    
+
+ }
+ else{
+    console.log("Computer is not bursting")
+
+ }
+ console.log("Your number is"+a)
