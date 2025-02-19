9a2bbeccceefa78a772841fd95e6f9c584849720
Smritee <esmritypoudel@gmail.com>
2025-02-19 14:17:21 +0530

    Add files via upload

---
+++ Loops.js
@@ -0,0 +1,36 @@
+console.log("I represent loop in js.")
+
+let a = 1;
+// console.log(a)
+// console.log(a+1)
+// console.log(a+2)
+
+// for (let i = 0; i < 100; i++) { 
+//     console.log(a + i); 
+// }
+
+// let obj = {
+//     name: "Smritee",
+//     role: "Programmer",
+//     company: "SalesForce"
+// }
+
+// for (const key in obj) {  
+//         console.log(key)
+// }
+
+// for (const c of "Smritee") {
+//     console.log(c)
+// }
+
+/*let i = 0;
+while (i < 60000) {
+    console.log(i)
+    i++;
+}*/
+
+let j = 10;
+do {
+    console.log(i)
+    j++;
+} while (i<6);
